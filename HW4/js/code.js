8d376d159bc9d6033b6bed52fc1f254b755c87b7
BrezhnevSergey <bsa_84@ukr.net>
2016-08-01 22:08:59 +0300

    +ADD HomeWork4-1 Fix

--- HW4/js/code.js
+++ HW4/js/code.js
@@ -1,7 +1,7 @@
 
 var str = "Lorem,ipsum,dolor,sit,amet,secte,uer,ad,scing,elit,sedam,nonumy,nibh,euismod,tdunt,ut,laoreet,dolmagna,quam,erat,volu,pat,Ut,wisi,ad,minim,veniam,quis,nostru,xerctation,ullamcorper,susc,lobortis,nis,ut,aliuip,ex,ea,consequat";
+console.log("Input string: " + str)
 var arr = str.split(',');
-console.log(arr);
 arr.sort();
 var newStr = arr.join(",");
-console.log(newStr);
+console.log("Output sort string: " + newStr);
