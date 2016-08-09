5130b334d08629d12c26cdfbdddf8c87a6e36a1e
BrezhnevSergey <bsa_84@ukr.net>
2016-08-09 19:06:48 +0300

    +ADD HomeWork6 add teacher's variant

--- HW6/js/code.js
+++ HW6/js/code.js
@@ -1,4 +1,3 @@
-
 function sum(num1) {
   var result = num1;
   function func(num2) {
@@ -9,5 +8,16 @@ function sum(num1) {
   return func;
 }
 
+//teacher
+function calc(arg) {
+	var sum	= arg;
+	return function(arg) {
+		return (arg) ? calc( sum += arg ) : sum;
+	}
+}
+
 console.log(sum(1)(2));
 console.log(sum(1)(2)(3)(4)(5)(6)(7)(8));
+
+console.log(calc(1)(2));
+console.log(calc(1)(2)(3)(4)(5)(6)(7)(8));
