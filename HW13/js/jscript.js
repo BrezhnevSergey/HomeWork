a9c7f6ab30cb46a2b2cf300800f2762e6bd1bcda
BrezhnevSergey <bsa_84@ukr.net>
2016-09-07 20:03:13 +0300

    +ADD HW13

--- HW13/js/jscript.js
+++ HW13/js/jscript.js
@@ -5,12 +5,8 @@ $(function() {
     var context = canvas.get(0).getContext("2d")
     var isDrag = false;
 
-    var cubes = []
+    var cubes = [];
     var currentCube = {};
-
-    
-    var xColl = false;
-    var yColl = false;
 
     canvas.on("mousedown", function(e) {
         isDrag = true;
@@ -34,6 +30,7 @@ $(function() {
             if(x >= cubes[index].x && x <= cubes[index].x + cubes[index].width) {
                 if (y >= cubes[index].y && y <= cubes[index].y + cubes[index].height) {
                     currentCube = cubes[index];
+                    currentCube.index = index;
                     currentCube.shiftX = x - cubes[index].x;
                     currentCube.shiftY = y - cubes[index].y;
                 }
@@ -42,24 +39,36 @@ $(function() {
     }
 
     function changeCoords(obj, x, y) {
-<<<<<<< HEAD
-        if ( ((x + cube.width) >= cube1.x) && (x <= (cube1.x + cube1.width)) ) { xColl = true; console.log("xCollision"); }
-        if ( ((y + cube.height) >= cube1.y) && (y <= (cube1.y + cube1.height)) ) { yColl = true; console.log("yCollision"); }
-        
-        if (xColl&&yColl){console.log("Crash!!!!");}
-            
-        obj.x = x;
-        obj.y = y;
-=======
         if(isDrag) {
             if ( (x - obj.shiftX >= 0) && (y - obj.shiftY >= 0) && 
                 ((x + (obj.width - obj.shiftX)) <= map.width) && ((y + (obj.height - obj.shiftY)) <= map.height) ) {
                 obj.x = x - obj.shiftX;
                 obj.y = y - obj.shiftY;
             }
+            collision(obj);
         }
->>>>>>> 9db0abf3acbf69a8107de26bf5d48f26b8300b6f
-        
+    }
+
+    function collision(obj) {
+        var XColl = false;
+        var YColl = false;
+        for (var i = 0; i < cubes.length; i++) {
+            if (i == obj.index) {
+                return;
+            } else {
+                if ( (obj.x + obj.width >= cubes[i].x) && (obj.x <= cubes[i].x + cubes[i].width) ) { 
+                    XColl = true;
+                }
+                if ( (obj.y + obj.height >= cubes[i].y) && (obj.y <= cubes[i].y + cubes[i].height) ) {
+                    YColl = true;
+                }
+                if(XColl && YColl) {
+                    console.log("obj")
+                    console.log("Crash obj[" + obj.index + "],obj.x=" + obj.x +",obj.y=" + obj.y + 
+                        " with obj[" + i + "],obj[" + i + "].x=" + cubes[i].x + ",obj[" + i + "].y=" + cubes[i].y);
+                }
+            }
+        }
     }
       
     function Entity(color, x, y, width, height) {
@@ -76,17 +85,19 @@ $(function() {
       
     function draw() {
         map.draw();
-        cube.draw();
-        cube1.draw();
+        for (var i = 0; i < cubes.length; i++) {
+            cubes[i].draw()
+        }
     }
       
     var map = new Entity("white", 0, 0, canvas.width(), canvas.height());
-    var cube = new Entity("red", 15, 15, 30, 30);
+    var cube0 = new Entity("red", 15, 15, 30, 30);
     var cube1 = new Entity("blue", 50, 50, 30, 30);
+    var cube2 = new Entity("green", 50, 110, 30, 30);
 
-    cubes.push(cube);
+    cubes.push(cube0);
     cubes.push(cube1);
+    cubes.push(cube2);
     
-    cube.draw(15, 15);
-    cube1.draw(50, 50);
+    draw();
 });
