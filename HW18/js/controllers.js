b999462a94eca93292eef7fb9bd0a6278e91231f
BrezhnevSergey <bsa_84@ukr.net>
2016-10-04 10:34:17 +0300

    +ADD HW18 start

---
+++ HW18/js/controllers.js
@@ -0,0 +1,15 @@
+
+var authApp = angular.module("authApp");
+
+authApp.controller('loginCtrl', function () {
+
+});
+
+authApp.controller('adminCtrl', function () {
+
+});
+
+authApp.controller('userCtrl', function () {
+
+});
+
